bda932d73f04212eee5ff6d4955dd57e82a80a15
Alex Colombo <uricaine@hotmail.com>
2021-01-30 19:24:23 +0100

    (done) add games

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,16 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+interface Favorito {
+  id: number;
+  nombre: string;
+}
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
 
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
   styleUrls: ['./dinamicos.component.css'],
 })
-export class DinamicosComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-
+export class DinamicosComponent {
+  @ViewChild('miFormulario') miForm!: NgForm;
+  nuevoJuego: string;
+  persona: Persona = {
+    nombre: 'Alex',
+    favoritos: [
+      { id: 1, nombre: 'Metal Gear' },
+      { id: 2, nombre: 'Ghost Recom' },
+    ],
+  };
   save() {
     console.log('save....');
+  }
+  agregarJuego() {
+    const nuevoFavorito: Favorito = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego,
+    };
+    this.persona.favoritos.push({ ...nuevoFavorito });
+    this.nuevoJuego = '';
+  }
+
+  eliminar(i: number) {
+    console.log(i);
+    this.persona.favoritos.splice(i, 1);
   }
 }
