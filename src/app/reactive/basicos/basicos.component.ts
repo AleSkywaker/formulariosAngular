e7277cde627dcba6a764320b60a7168056dbd9d2
Alex Colombo <uricaine@hotmail.com>
2021-02-02 21:20:06 +0100

    new features

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html',
   styleUrls: ['./basicos.component.css'],
 })
-export class BasicosComponent {
+export class BasicosComponent implements OnInit {
   // miFormulario: FormGroup = new FormGroup({
   //   nombre: new FormControl('RTX 4080ti'),
   //   precio: new FormControl(1500),
@@ -14,9 +19,43 @@ export class BasicosComponent {
   // });
 
   miFormulario: FormGroup = this.fb.group({
-    nombre: ['RTX 4080ti'],
-    precio: [1800],
-    existencias: [10],
+    nombre: [, [Validators.required, Validators.minLength(3)]],
+    precio: [, [Validators.required, Validators.min(0)]],
+    existencias: [, [Validators.required, Validators.min(0)]],
   });
   constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    //* mejor usar reset que setvalue, porque todos los parametros no son obligatorios
+    this.miFormulario.reset({
+      nombre: 'Alex',
+      precio: 10,
+    });
+  }
+
+  validacion() {
+    if (
+      this.miFormulario.controls.nombre.value ||
+      this.miFormulario.controls.precio.value ||
+      this.miFormulario.controls.existencias.value
+    ) {
+      return true;
+    }
+    return false;
+  }
+  guardar() {
+    if (this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+    console.log(this.miFormulario.value);
+    this.miFormulario.reset();
+  }
+
+  campoNoEsValido(campo: string) {
+    return (
+      this.miFormulario.controls[campo].errors &&
+      this.miFormulario.controls[campo].touched
+    );
+  }
 }
